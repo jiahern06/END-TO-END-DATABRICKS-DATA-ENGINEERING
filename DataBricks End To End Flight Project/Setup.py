8e27058a5402872366cea8ece590726662cb2ce9
jiahern06 <alvinwongjh2006@gmail.com>
2026-08-17 19:35:34 +0800

    Delete DataBricks End To End Flight Project directory

--- DataBricks End To End Flight Project/Setup.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC CREATE VOLUME workspace.raw.rawvolume
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC SELECT * FROM delta.`/Volumes/workspace/bronze/bronzevolume/flights/data/`
